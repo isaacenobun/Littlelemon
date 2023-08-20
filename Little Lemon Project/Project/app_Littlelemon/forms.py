2b0497c7335c9569a3a37c251448c74f1a09f157
isaacenobun <76211324+isaacenobun@users.noreply.github.com>
2023-08-20 22:38:19 +0100

    Django files

---
+++ Little Lemon Project/Project/app_Littlelemon/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Booking
+
+
+# Code added for loading form data on the Booking page
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = "__all__"
